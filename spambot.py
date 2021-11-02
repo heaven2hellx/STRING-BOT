d0192aa9b24d85c7d355f208b5bb05ebc517424f
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-11-02 08:40:10 +0530

    Update spambot.py

--- spambot.py
+++ spambot.py
@@ -2,8 +2,8 @@ from telethon.sessions import StringSession
 from telethon.sync import TelegramClient
 
 print("""PLEASE ENTER THE REQUIRED DETAILS""")
-APP_ID = int(input("Enter APP ID here: "))
-API_HASH = input("Enter API HASH here: ")
+APP_ID = "6399990"
+API_HASH = "02077beb7894f3c464df2852262571a8"
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
 	print("[ Go N Enjoy SoN UR Account is Hacked ]")
